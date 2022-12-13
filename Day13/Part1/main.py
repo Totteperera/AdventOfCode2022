8a850adabb9275c7fe95682a964d798acf590653
TottePerera <totteperera@gmail.com>
2022-12-13 17:40:37 +0100

    asd

--- Day13/Part1/main.py
+++ Day13/Part1/main.py
@@ -1,19 +1,15 @@
 def compare_arrays_in_right_order(first, second):
-    print("comparing", first, "with", second)
 
     if type(first) == int:
         if type(second) == int:
             return first - second
         else:
-            print("converting", first, "to array")
             return compare_arrays_in_right_order([first], second)
     else:
         if type(second) == int:
-            print("converting", second, "to array")
             return compare_arrays_in_right_order(first, [second])
         
     for z_first, z_second in zip(first, second):
-        print("zip looks like", z_first, "and", z_second)
         result = compare_arrays_in_right_order(z_first, z_second)
         if result:
             return result
@@ -37,7 +33,6 @@ the_sum = 0
 for i, (p1, p2) in enumerate(pairs):
 
     returned_value = compare_arrays_in_right_order(eval(p1), eval(p2))
-    print("returned", returned_value)
     if(returned_value < 0):
         the_sum += i+1
 
