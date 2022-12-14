b3c700ac48924166a70f3bcf0c4a27de99b4c800
TottePerera <totteperera@gmail.com>
2022-12-14 10:57:53 +0100

    part 2

--- Day14/Part2/main.py
+++ Day14/Part2/main.py
@@ -22,6 +22,11 @@ for row in input:
         old = (x,y)
 
 lowest_point = max([x[1] for x in mat])
+bottom_y = lowest_point + 2
+
+for i in range(100000):
+    mat.add((i,bottom_y))
+
 is_full = False
 nr_of_sand = 0
 while not is_full:
@@ -29,22 +34,22 @@ while not is_full:
     is_set = False
     should_add_to_mat = True
     sx, sy = (500, 0)
+
+    if (sx, sy) in mat:
+        is_full = True
+
     while not is_set:
         for nx,ny in [(sx, sy + 1), (sx - 1, sy + 1), (sx + 1, sy + 1)]:
             if (nx,ny) not in mat:
                 sx = nx
                 sy = ny
                 should_add_to_mat = False
-
-                if ny >= lowest_point:
-                    is_full = True
-                    is_set = True
-
                 break
 
             if (nx, ny) in mat:
                 should_add_to_mat = True
                 continue
+
 
         if should_add_to_mat:
             mat.add((sx,sy))
