67f64d6bf915caa9327008bcdca5967a000e23ca
TottePerera <totteperera@gmail.com>
2022-12-25 11:17:38 +0100

    part1

--- Day18/Part1/main.py
+++ Day18/Part1/main.py
@@ -3,27 +3,25 @@ filename = sys.argv[1]
 
 lines = [x.split(",") for x in open(filename).read().split("\n")]
 
-a = [1,2,3]
-b = [2,2,3]
-c = zip(a,b)
+index = [[] for x in range(len(lines))]
+print(index)
 
-index = {}
+for li, line in enumerate(lines):
 
-for li,line in enumerate(lines):
-    x,y,z = line
+    x, y, z = line
 
     for other_li, other_line in enumerate(lines):
         if other_li == li:
             continue
 
         matches = [set(x) for x in zip(line, other_line)]
-        
+
         if len(list(filter(lambda x: len(x) == 1, matches))) == 2:
-            index[li] = index[li].append(li)
-            # we have hit, save to index
 
-        ox,oy,oz = other_line
+            not_same = tuple(list(filter(lambda x: len(x) == 2, matches))[0])
+            if abs(int(not_same[0]) - int(not_same[1])) == 1:
+                index[li].append(other_li)
 
-        matching = 0
+sides = sum([len(x) for x in index])
 
-print(index)
+print((len(lines))*6 - sides)
